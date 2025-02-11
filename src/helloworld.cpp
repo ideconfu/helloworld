7057a3326d61dfc2d190403763e2bc64425c136e
claudius li <lipb_2021@163.com>
2025-02-11 16:38:21 +0800

    add operation interface and print method

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -5,6 +5,11 @@
 #include <iostream>
 #include <memory>
 
+void print()
+{
+    std::cout << "hello world" << std::endl;
+}
+
 int main()
 {
     boost::asio::io_context io;
@@ -19,12 +24,21 @@ int main()
         std::cout << "lipb debug : " << mode << std::endl;
     });
 
+    std::shared_ptr<sdbusplus::asio::dbus_interface> helloifc =
+        objectServer.add_interface("/xyz/openbmc_project/helloworld/operations",
+                                   "xyz.openbmc_project.helloworld.operations");
+
+    helloifc->register_method("print", print);
+
+    helloifc->initialize();
+
     conn->request_name("xyz.openbmc_project.helloworld");
 
     auto hello_ptr =
         std::make_unique<Power>(conn, "/xyz/openbmc_project/helloworld/power");
 
-    hello_ptr->powerStatus(static_cast<PowerStatusInterface::PowerStatusValues>(1));
+    hello_ptr->powerStatus(
+        static_cast<PowerStatusInterface::PowerStatusValues>(1));
     hello_ptr->name("CRPS2000NC");
 
     io.run();
